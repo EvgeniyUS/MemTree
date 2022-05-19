423d9de40aee3886516e3b83e2f11802ce6aef2e
e.us <e.us@mashtab.org>
2022-05-19 11:14:42 +0300

    static

--- MySite/settings.py
+++ MySite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't@4ss2uj6at^!w0(fyen9%=#j5sd6r)q&7#0+@6^g7*v@ffg#x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [u'127.0.0.1',
                  # u'192.168.224.205',
@@ -120,5 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'MySite/static/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'MySite/static/'),
+                    os.path.join(BASE_DIR, 'MemTree/static/')]
 LOGIN_URL = '/login'
