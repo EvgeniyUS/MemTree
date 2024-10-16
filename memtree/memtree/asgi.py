5663e65a18785c6b06bf866f5d84a3c637b95e1e
eus <e.us@spacevm.org>
2024-10-17 01:49:05 +0300

    ansible wip - its alive!

--- memtree/memtree/asgi.py
+++ memtree/memtree/asgi.py
@@ -3,10 +3,11 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from . import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'memtree.settings')
 django_asgi_app = get_asgi_application()
+
+from . import routing  # NOQA
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
