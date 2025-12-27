24fea13c500eb123c3c0b9f49359f8fa0d9d5e76
e.us <e.us@spacevm.org>
2025-12-27 14:23:31 +0300

    finished field first for mobile use

--- memtree/task/admin.py
+++ memtree/task/admin.py
@@ -4,13 +4,13 @@ from .models import Task
 
 class TaskAdmin(admin.ModelAdmin):
     list_display = (
-        'id',
-        'name',
-        'user',
-        'created',
         'finished',
-        'result',
+        'name',
         'status',
+        'user',
+        'id',
+        'created',
+        'result',
     )
 
 
